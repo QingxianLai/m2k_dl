dbfd9814408cc699701fd525291e89821ece9df4
QingxianLai <ql516@nyu.edu>
2016-03-04 00:31:58 -0500

    surrogate data preprocessing rewrite

--- assignment2/surrogate/data.lua
+++ assignment2/surrogate/data.lua
@@ -4,18 +4,34 @@ require 'xlua'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
-local Provider = torch.class 'Provider'
 
 local function _rand(max_val)
     return math.floor(torch.rand(1)[1] * max_val + 1)
 end
 
-function data_augmentation(src, width, height, max_expand, max_rotate, max_shift)
+local function _balance_rand()
+    return torch.rand(1)[1] * 2 - 1
+end
+
+function _contrast(src, c)
+    F = (259 * (c + 225)) /(255 * (259 - c))
+    local new_image = torch.Tensor(src:size()):copy(src)
+    for i = 1, src:size()[1] do
+        local mean = src[i]:mean()
+        new_image[i]:add(-mean):mul(F):add(mean)
+        new_image[i]:clamp(-255,255)
+    end
+    return new_image
+end
+
+
+function data_augmentation(src, width, height, max_expand, max_rotate, max_shift, max_contrast)
     -- expand
-    local _expand = image.scale(_rand(width * max_expand))
-    local x1 = _rand(_expand - width)
-    local y1 = _rand(_expand - height)
-    src = image.crop(src, x1, y1, x1+width, y1 + height) 
+    local exVal =  width * _rand(max_expand)
+    local _expand = image.scale(src, exVal)
+    local x1 = _rand(exVal - width-1)
+    local y1 = _rand(exVal - height-1)
+    src = image.crop(_expand, x1, y1, x1+width, y1 + height) 
 
     -- shift
     src = image.translate(src, max_shift * _balance_rand(), max_shift * _balance_rand())
@@ -23,12 +39,14 @@ function data_augmentation(src, width, height, max_expand, max_rotate, max_shift
     -- rotate
     src = image.rotate(src, max_rotate * _balance_rand())
 
-    -- contrast1
-    
+    -- contrast
+    src = _contrast(src, max_contrast*_balance_rand()) 
 
-    -- contrast2
     return src
 end
+
+
+local Provider = torch.class 'Provider'
 
 function Provider:__init(full)
     local exsize = 100000
@@ -36,7 +54,7 @@ function Provider:__init(full)
     local height = 96
     local width = 96
     
-    local psize = 400
+    local psize = 4000
     local K = 150 --augmented versions for each patch
     local pheight = 32
     local pwidth = 32
@@ -44,6 +62,7 @@ function Provider:__init(full)
     if not paths.dirp('stl-10') then 
         print("fail to read data")
         return
+    end
 
     local raw_extra = torch.load('./stl-10/extra.t7b')
 
@@ -59,10 +78,33 @@ function Provider:__init(full)
     local idx = 1
     for i = 1, psize do
         local this_d = raw_extra[_rand(exsize)]
+        this_d = this_d:float()
         local x1 = _rand(width - pwidth)
         local y1 = _rand(height - pheight)
         local patch = image.crop(this_d, x1, y1, x1+pwidth, y1+pheight)
         t[idx]:copy(patch)
         l[idx] = idx
-        
+        for j = 1,K-1 do
+            local new_patch = data_augmentation(patch,pwidth,pheight,1.3,0.2,3,150)
+            t[j*K + idx]:copy(new_patch)
+            t[j*K + idx] = idx
+        idx = idx + 1
+        end
+    end
+    self.unlabelData.data = t
+    self.unlabelData.labels = l
 
+    self.unlabelData.size = function() return psize * K end
+
+end
+
+function Provider:normalize()
+    local data = self.unlabelData
+    print '<trainer> preprocessing data (normalization)'
+    collectgarbage()
+
+    for i = 1,3 do
+        local mean = data.data:select(2,i):mean()
+        data.data:select(2,i):add(-mean)
+    end
+end
