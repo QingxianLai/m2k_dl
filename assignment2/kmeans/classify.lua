b88271a8b93323242c1cb1d82d334e40e7dd5d34
QingxianLai <ql516@nyu.edu>
2016-03-07 05:37:37 +0000

    modify kmeans

--- assignment2/kmeans/classify.lua
+++ assignment2/kmeans/classify.lua
@@ -2,7 +2,9 @@ require 'xlua'
 require 'nn'
 require 'image'
 require 'torch'
-local c = require 'treql.colorize'
+require 'cunn'
+require 'cutorch'
+local c = require 'trepl.colorize'
 
 opt = lapp[[
     -l,--labels (default "./labels.t7")    cluster label of the extra data
@@ -13,17 +15,23 @@ print (opt)
 local channel = 3
 local height = 96
 local width = 96
+local extraSize = 5000
+-- load index
+local index = torch.load('./index.t7')
 
 
 -- read in data and convert it to one tensor : data
 print(c.blue '==>' ..' loading extra data')
 local raw = torch.load('./stl-10/extra.t7b')
-data = torch.FloatTensor(100000, channel, width, height)
+data = torch.FloatTensor(extraSize, channel, width, height)
 local idx = 1
-for i = 1, #raw.data[1] do
-    data[idx]:copy(raw.data[1][i]:reshape(1, channel, width, height))
+for i = 1, extraSize do
+    data[idx]:copy(raw.data[1][index[i]]:reshape(1, channel, width, height))
     idx = idx + 1
 end
+
+raw = nil
+collectgarbage()
 
 
 -- change color space and normalize the extra dataset
@@ -33,18 +41,19 @@ local std_u = 12.419259853307
 local mean_v = 1.4553072717114
 local std_v = 15.0061438658
 for i=1,data:size()[1] do
-    xlua.progress(i, #raw.data[1])
-    local rgb = data[i]
+    xlua.progress(i, data:size()[1])
+    local rgb = data[i]:double()
     local yuv = image.rgb2yuv(rgb)
     -- nomoralize y locally
     yuv[{1}] = normalization(yuv[{{1}}])
-    data[i] = yuv
+    data[i] = yuv:float()
 end
 data:select(2,2):add(-mean_u)
 data:select(2,2):div(std_u)
 data:select(2,3):add(-mean_v)
 data:select(2,3):add(std_v)
 
+collectgarbage()
 
 -- loading labels, put data into different groups
 print(c.blue '==>' ..' loading labels')
@@ -52,11 +61,12 @@ local raw_l = torch.load(opt.labels)
 print(raw_l:size())
 clusters = {}
 for i=1,10 do
-    cluster[i] = {}
+    clusters[i] = {}
 end
-assert(#raw_l == data:size()[1], "label size not equal to sample size")
+assert((#raw_l)[1] == data:size()[1], string.format("label size %s not equal to sample size%s ", (#raw_l), data:size()[1]))
 for i=1,raw_l:size()[1] do
-    table.insert(cluster[i], data[i]:clone())
+    xlua.progress(i, raw_l:size()[1])
+    table.insert(clusters[raw_l[i]], data[i]:clone())
 end
 
 -- release memory
@@ -79,17 +89,33 @@ result={
 
 for i=1,10 do
     local cluster = clusters[i]
-    local thresh = math.ceil(0.6 * #cluster)
-    local prediction = torch.FloatTensor(10, #cluster):zero()
-    for j=1,#cluster do
-        local output = model.forward(cluster[j])
-        prediction[output][j] = 1
+    local thresh = math.ceil(0.3 * #cluster)
+    local batchSize = 64
+    local pred_all = torch.FloatTensor(#cluster)
+    local pred_sum = torch.FloatTensor(10):zero()
+
+    for j = 1,#cluster,batchSize do
+        xlua.progress(j, #cluster)
+        local lastj = math.min(#cluster, j+batchSize-1)
+        local m = lastj - j + 1
+        local t = torch.FloatTensor(m, 3, 96, 96)
+        for k = 0,m-1 do
+            t[k+1]:copy(cluster[j+k])
+        end
+        local output = model:forward(t:cuda())
+        local val, pred = torch.max(output, 2)
+        pred_all[{{j,lastj}}]:copy(pred)
     end
-    local pred_sum = torch.sum(prediction, 2)
+
+    for j = 1, #cluster do
+        pred_sum[pred_all[j]] = pred_sum[pred_all[j]] + 1
+    end
+    print(pred_sum)
+    
     for j = 1,10 do
-        if pred_sum >= thresh then
+        if pred_sum[j] >= thresh then
             for k = 1, #cluster do
-                if prediction[j][k] == 1 then
+                if pred_all[k] == j then
                     table.insert(result.data, cluster[k])
                     table.insert(result.labels, j)
                 end
@@ -98,11 +124,8 @@ for i=1,10 do
         end
     end
 end
-
 print(#result.data)
-torch.save("new_data.t7t", result)
-
-
+--torch.save("new_data.t7t", result)
 
 
 
--- assignment2/kmeans/train.lua
+++ assignment2/kmeans/train.lua
@@ -4,12 +4,12 @@ require 'unsup'
 local c = require 'trepl.colorize'
 
 opt = lapp[[
-   -d,--data                   (default "train")      input dataset
+   -d,--data                   (default "extra")      input dataset
    -s,--save                  (default "logs")        subdirectory to save logs
    -b,--batchSize             (default 64)            batch size
    -k,--kclass                (default 10)            number of clusters
    -m,--model                    (default kmeans)        model name
-   -i,--max_iter                 (default 100)           maximum number of iterations
+   -i,--max_iter                 (default 200)           maximum number of iterations
 ]]
 
 print(opt)
@@ -17,6 +17,7 @@ print(opt)
 local channel = 3
 local height = 96
 local width = 96
+
 
 print(c.blue '==>' ..' loading data')
 if opt.data == 'train' then
@@ -30,14 +31,18 @@ if opt.data == 'train' then
         end
     end
 elseif opt.data == 'extra' then
+    io.write("extra dataset")
     local raw = torch.load('./stl-10/extra.t7b')
-    data = torch.FloatTensor(100000, channel * width * height)
-    local idx = 1
-    for i = 1, #raw.data[1] do
-        data[idx]:copy(raw.data[1][i]:reshape(1, channel * width * height))
-        idx = idx + 1
+    data = torch.FloatTensor(5000, channel * width * height)
+    local index = torch.randperm(100000)[{{1,5000}}]
+    torch.save("index.t7", index)
+    for i = 1, 5000 do
+        xlua.progress(i, 5000)
+        data[i]:copy(raw.data[1][index[i]]:reshape(1, channel * width * height))
     end
 end
+raw = nil
+collectgarbage()
 
 data = data:float()
 
