f5cf4caa67e8e98635de3e9d5d177a5729f2b03d
QingxianLai <ql516@nyu.edu>
2016-03-13 20:40:25 +0000

    filter of experiemnts with unlabel data

--- assignment2/showFilter.lua
+++ assignment2/showFilter.lua
@@ -2,9 +2,9 @@ require 'cunn'
 require 'cutorch'
 require 'image'
 
-local model = torch.load("./surrogate/logs/surrogate/model.net")
+local model = torch.load("./default_add_data/logs/model.net")
 
 local w = model:get(1).weight
 print(w:size())
 dis = image.toDisplayTensor{input=w, padding=1, nrow=8}
-image.save("./filters/surrogate.png", dis)
+image.save("./filters/baseline_add_data.png", dis)
