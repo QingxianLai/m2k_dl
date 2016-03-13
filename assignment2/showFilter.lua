9d5808150bdeb777ee5cc4ffcfb4ce0bc8b9caa0
QingxianLai <ql516@nyu.edu>
2016-03-13 20:36:26 +0000

    surrogate class filter

--- assignment2/showFilter.lua
+++ assignment2/showFilter.lua
@@ -2,9 +2,9 @@ require 'cunn'
 require 'cutorch'
 require 'image'
 
-local model = torch.load("./default/logs/vgg_da/model.net")
+local model = torch.load("./surrogate/logs/surrogate/model.net")
 
 local w = model:get(1).weight
 print(w:size())
 dis = image.toDisplayTensor{input=w, padding=1, nrow=8}
-image.save("./filters/augmentation.png", dis)
+image.save("./filters/surrogate.png", dis)
