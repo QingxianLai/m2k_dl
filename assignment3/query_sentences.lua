3b81c7765816abaaebc0fb80e24a962e0eb1165c
QingxianLai <ql516@nyu.edu>
2016-05-02 21:10:16 -0400

    query_sentence.lua v1.0 :

--- assignment3/query_sentences.lua
+++ assignment3/query_sentences.lua
@@ -1,18 +1,26 @@
 stringx = require('pl.stringx')
 require 'io'
+require 'nngraph'
+require 'base'
+
 
 -- read in data and dictionaries
 data = require('data')
 _ = data.traindataset(20)
-
+vocab_map = data.vocab_map
+vocab_rmap = data.rmap
 
 -- read std in
 function readline()
     local line = io.read("*line")
     if line == nil then error({code="EOF"}) end
     line = stringx.split(line)
-    if tonumber(line[1]) == nil then error({code="EOF"}) end
-    return line
+    if tonumber(line[1]) == nil then error({code="init"}) end
+    num = tonumber(line[1])
+    words = {}
+    for i = 2,#line do
+        words[i-1] = line[i]:lower()
+    return {num, words}
 end
 
 
@@ -21,33 +29,86 @@ model_file = "lstm_model.obj"
 model = torch.load(model_file)
 
 
+
+local function reset_state()
+    if model ~= nil and model.start_s ~= nil then
+        for d = 1, 2 * params.layers do
+            model.start_s[d]:zero()
+        end
+    end
+end
+
+
+batch_size = 20
+
+local function encode(word)
+    local id = nil
+    if vocab_map[word] ~= nil then
+        id = vocab_rmap[word]
+    else
+        id = vocab_rmap["<unk>"]
+    end
+    local batch = torch.Tensor(batch_size):fill(id)
+    return batch
+end
+
+
+local function multi(pred)
+    return torch.multinomial(torch.exp(pred), 1, true)[1]
+end
+
+
+local function query(num, words)
+    reset_state()
+    g_disable_dropout(model.rnns)
+    local result = {}
+    
+    g_replace_table(model.s[0], model.start_s)
+    for i = 1, #words do
+        local x = encode(words[i])
+        local y = x
+        _, model.s[1], pred = unpack(model.rnns[1]:forward({x,y,model.s[0]}))
+        result[i] = words[i]
+        g_replace_table(model.s[0], model.s[1])
+    end
+    
+    next_word = multi(pred[1])
+
+    for i = 1,num do   
+        x = torch.Tensor(batch_size):fill(next_word)
+        local y = x
+        _, model.s[1], pred = unpack(model.rnn[1]:forward({x, y, model.s[0]}))
+        next_word = multi(pred[1])
+        result[i+#words] = vocab_rmap[next_word]
+        g_replace_table(model.s[0], model.s[1])
+    end
+    g_enable_dropout(model.rnns)
+    return results
+end
+
+
 -- std in out loop
 while true do
     print("Query: len word1 word2 etc")
     local ok, line = pcall(readline)
     if not ok then
-        print(line)
+        if line.code == "EOF" then
+            break
+        elseif line.code == "init" then
+            print("not a number")
+        else
+            print(line)
+        end
     else
-        -- do something here
+        print("sentence: ")
+        num = line[1]
+        words = line[2]
+        extra = query(num, words)
+        for i = 1, #extra do
+            io.write(extra[i] .. " ")
+        end
+        io.write("\n")
     end
 end
 
 
-function run_test()
-    reset_state(state_test)
-    g_disable_dropout(model.rnns)
-    local perp = 0
-    local len = state_test.data:size(1)
-    
-    -- no batching here
-    g_replace_table(model.s[0], model.start_s)
-    for i = 1, (len - 1) do
-        local x = state_test.data[i]
-        local y = state_test.data[i + 1]
-        perp_tmp, model.s[1] = unpack(model.rnns[1]:forward({x, y, model.s[0]}))
-        perp = perp + perp_tmp[1]
-        g_replace_table(model.s[0], model.s[1])
-    end
-    print("Test set perplexity : " .. g_f3(torch.exp(perp / (len - 1))))
-    g_enable_dropout(model.rnns)
-end
