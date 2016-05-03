977779ff13e48a63e3849b8a445694f35f67610c
QingxianLai <ql516@nyu.edu>
2016-05-03 02:00:25 -0400

    add nngraph_warmup.lua

--- assignment3/nngraph_warmup.lua
+++ assignment3/nngraph_warmup.lua
@@ -1,24 +1,48 @@
-require 'torch'
 require 'nn'
 require 'nngraph'
 
-x = nn.Identity()()
-y = nn.Identity()()
-l1_x = nn.Tanh()(nn.Linear(4,2)(x))
-l1_y = nn.Sigmoid()(nn.Linear(5,2)(y))
-z = nn.Identity()()
-l2 = nn.CMulTable()({l1_x, l1_y})
-a = nn.CAddTable()({l2, z})
+local x = nn.Identity()()
+local y = nn.Identity()()
+local z = nn.Identity()()
 
-g = nn.gModule({x,y,z},{a})
+local l1_x = nn.Tanh()(nn.Linear(4,2)(x))
+local l1_y = nn.Sigmoid()(nn.Linear(5,2)(y))
+
+local sl1_x = nn.Square()({l1_x})
+local sl1_y = nn.Square()({l1_y})
+
+local l2 = nn.CMulTable()({sl1_x,sl1_y})
+
+local a = nn.CAddTable()({l2, z})
+
+local g = nn.gModule({x,y,z},{a})
 
 
-x_data = torch.rand(4)
-y_data = torch.rand(5)
-z_data = torch.rand(2)
-gradOutput = torch.ones(2)
+local x_data = torch.rand(4)
+local y_data = torch.rand(5)
+local z_data = torch.rand(2)
+local gradOutput = torch.ones(2)
 
-g:updateOutput({x_data, y_data, z_data})
-g:updateGradInput({x_data, y_data, z_data}, gradOutput)
+--g:updateOutput({x_data, y_data, z_data})
+--g:updateGradInput({x_data, y_data, z_data}, gradOutput)
 
+-- demo
+print("x=")
+print(x_data)
+print("y=")
+print(y_data)
+print("z=")
+print(z_data)
+
+print("Forward Propagating:")
 print(g:forward({x_data, y_data, z_data}))
+
+print("GradOutput:")
+print(gradOutput)
+
+print("back propagate w.r.t. x")
+print(g:backward({x_data, y_data, z_data}, gradOutput)[1])
+print("back propagate w.r.t. y")
+print(g:backward({x_data, y_data, z_data}, gradOutput)[2])
+print("back propagate w.r.t. z")
+print(g:backward({x_data, y_data, z_data}, gradOutput)[3])
